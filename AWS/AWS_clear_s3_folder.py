05f300732e99d0871b8a8c7d3c1ba50fde8affed
Ivan <ivanzheng1122@gmail.com>
2025-05-01 10:06:36 +1000

    update

--- AWS/AWS_clear_s3_folder.py
+++ AWS/AWS_clear_s3_folder.py
@@ -1,6 +1,10 @@
+import sys
+import os
+parent_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(1, parent_path)  # caution: path[0] is reserved for script path (or '' in REPL)
+
 from utils.AWS_functions import delete_folder_content
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 s3_folder = os.getenv("AWS_S3_FOLDER")
